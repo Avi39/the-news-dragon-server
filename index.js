e97085e1dc6b961e48109bcf11b40cbc628c85ed
Avi <avishekuser@gmai.com>
2023-05-08 04:36:12 +0600

    vercel

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const port =  process.env.PORT || 5000;
 const catagories = require('./data/catagories.json')
-
+const news = require('./data/news.json')
 app.use(cors());
 
 app.get('/',(req,res)=>{
@@ -11,8 +11,27 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/catagories',(req,res)=>{
-    console.log(catagories);
     res.send(catagories);
+})
+
+app.get('/news',(req,res) => {
+    res.send(news);
+})
+app.get('/news/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectedNews = news.find(n=>n._id===id);
+    res.send(selectedNews);
+})
+app.get('/catagories/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    if(id === 0){
+        res.send(news);
+    }
+    else{
+        const catagoryNews = news.filter(n=>parseInt(n.category_id)  === id)
+        res.send(catagoryNews);
+    }
+    
 })
 
 app.listen(port,()=>{
